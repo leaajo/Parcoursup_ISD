35f627a2feb3a058883cde4052b6e1e80f76cce3
leaajo <lea.joseph@icloud.com>
2024-03-27 19:50:39 +0100

    .

--- dataviz.py
+++ dataviz.py
@@ -10,42 +10,27 @@ sns.set_theme() # permet d'obtenir le fonc gris avec les lignes blanches
 
 import geopandas as gpd 
 
-from sklearn.impute import KNNImputer
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import cross_val_score, cross_val_predict, KFold
-from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.inspection import permutation_importance
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-import sklearn.metrics as confusion_matrix
-
-from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
-
 sns.axes_style(style=None, rc=None);
 
-url1 = "https://github.com/leaajo/TP_ISD/blob/master/all_df/fr-en-adresse-et-geolocalisation-etablissements-premier-et-second-degre.csv"
-url2 = "https://github.com/leaajo/TP_ISD/all_df/fr-en-lycee_gt-effectifs-niveau-sexe-lv.csv"
+url1 = "https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-en-adresse-et-geolocalisation-etablissements-premier-et-second-degre.csv"
+url2 = "https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-en-lycee_gt-effectifs-niveau-sexe-lv.csv"
 
-tous_les_etablissements = pd.read_csv(r"https://github.com/leaajo/TP_ISD/blob/master/all_df/fr-en-adresse-et-geolocalisation-etablissements-premier-et-second-degre.csv",sep=";")
-lycees = pd.read_csv(r"https://github.com/leaajo/TP_ISD/all_df/fr-en-lycee_gt-effectifs-niveau-sexe-lv.csv",sep=";")
+tous_les_etablissements = pd.read_csv(url1,sep=";")
+lycees = pd.read_csv(url2,sep=";")
 
+# On va fusionner les deux dataframes pour avoir les informations sur les lycées
 lycees_2 = lycees.copy()
 lycees_2 = lycees_2.merge(tous_les_etablissements, left_on="numero_lycee", right_on="numero_uai")
-
 # Liste des colonnes à conserver
 colonnes_a_garder = ['rentree_scolaire', 'denomination_principale_x', 'secteur', 'nombre_d_eleves', 'latitude', 'longitude', 'appariement', 'nature_uai_libe']
-
-# Sélectionner les colonnes spécifiées dans lycees_2
 lycees_2 = lycees_2.loc[:, colonnes_a_garder]
 
-# On va passer la latitude et la longitude en geometry (pour les cartes)
 
+# On va passer la latitude et la longitude en geometry (pour les cartes)
 # Conversion
 geometry = gpd.points_from_xy(lycees_2["longitude"], lycees_2["latitude"])
 
-# Create a DataFrame with a geometry containing the Points
+# Création du GeoDataFrame
 geo_lycee = gpd.GeoDataFrame(
     lycees_2, crs="EPSG:4326", geometry=geometry
 )
@@ -53,19 +38,18 @@ geo_lycee = gpd.GeoDataFrame(
 geo_lycee_metropole = geo_lycee[(geo_lycee['latitude'] > 40) &
                                 (geo_lycee['longitude']> -10)]
 
-url3 = "https://github.com/leaajo/TP_ISD/all_df/metropole.geojson"
+# On va charger les données géographiques de la France
+url3 = "https://raw.githubusercontent.com/leaajo/TP_ISD/master/donnees_geo/metropole.geojson"
 donnees_france = gpd.read_file(url3)
 
 # Créer une figure et des axes
 fig, ax = plt.subplots(figsize=(10, 10))
-
-ax.set_facecolor('white')
-
 # Tracer la carte de la France
 donnees_france.plot(ax=ax, color='lightgrey', edgecolor = 'grey')
-
 # Tracer les points des lycées sur la carte de la France
 geo_lycee_metropole.plot(ax=ax, color='blue', markersize=8)
 
-# Afficher la carte
+# Ajouter le titre
+ax.set_title("Répartition des lycées en France métropolitaine")
+
 plt.show()
--- librairies.py
+++ librairies.py
@@ -1,3 +1,4 @@
+import csv
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -7,18 +8,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_theme() # permet d'obtenir le fonc gris avec les lignes blanches
 
+import geopandas as gpd 
+
 from sklearn.impute import KNNImputer
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import cross_val_score, cross_val_predict, KFold
-from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import (
+    cross_val_score, 
+    cross_val_predict, 
+    train_test_split, 
+    GridSearchCV, 
+    KFold
+)
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.inspection import permutation_importance
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 import sklearn.metrics as confusion_matrix
-
 from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
-
-sns.axes_style(style=None, rc=None);
 
