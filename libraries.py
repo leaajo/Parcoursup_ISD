643f16868ee938eafd274f2249e34ab0607c3296
leaajo <lea.joseph@icloud.com>
2024-03-27 15:22:21 +0100

    okkkkkk

--- libraries.py
+++ libraries.py
@@ -5,18 +5,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+sns.set_theme() # permet d'obtenir le fonc gris avec les lignes blanches
+
 from sklearn.impute import KNNImputer
-import math
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_val_score, cross_val_predict, KFold
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.inspection import permutation_importance
-
-
-sns.set_theme() # permet d'obtenir le fonc gris avec les lignes blanches
-
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import sklearn.metrics as confusion_matrix
