b7e5945e820735016de3bc876bb6b9e8153fbcf3
leaajo <lea.joseph@icloud.com>
2024-03-28 16:20:44 +0100

    Ajout des dataframes de parcoursup pour les années 2018, 2019 et 2020

--- gros_df_parcoursup_toutes_annees.py
+++ gros_df_parcoursup_toutes_annees.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
+annee_2018 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup_2018.csv', sep=';')
+annee_2019 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup_2019.csv', sep=';')
 annee_2020 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup_2020.csv', sep=';')
-annee_2020 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup_2020.csv', sep=';')
-annee_2020 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup_2020.csv', sep=';')
+annee_2021 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup_2021.csv', sep=';')
+annee_2023 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursupcsv', sep=';')
+
+parcoursup_total = pd.concat([annee_2018, annee_2019, annee_2020, annee_2021, annee_2023])  # On concatène les données
+parcoursup_total.to_csv('parcoursup_total.csv', index=False) # Sauvegarde du DataFrame
