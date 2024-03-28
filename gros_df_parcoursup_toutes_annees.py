27fe2fe3a039ff8ff6b1499c7cd4dc84f31c7dab
leaajo <lea.joseph@icloud.com>
2024-03-28 16:17:33 +0100

    Toutes les données PSUP ajoutés de 2018 à 2023

--- gros_df_parcoursup_toutes_annees.py
+++ gros_df_parcoursup_toutes_annees.py
@@ -1,3 +1,5 @@
 import pandas as pd
 
-annee_2020 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup.csv', sep=';')
+annee_2020 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup_2020.csv', sep=';')
+annee_2020 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup_2020.csv', sep=';')
+annee_2020 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup_2020.csv', sep=';')
