7127ec3102f35cc5943e38e41d5b5bf69236eb41
leaajo <lea.joseph@icloud.com>
2024-03-28 19:34:19 +0100

    .

--- test_creations/gros_df_parcoursup_toutes_annees.py
+++ test_creations/gros_df_parcoursup_toutes_annees.py
@@ -3,9 +3,14 @@ import pandas as pd
 # Avec la réforme du baccalauréat, les données de Parcoursup ont été modifiées depuis 2021. Nous allons donc concaténer les données de 2021,2022 et 2023 pour avoir un DataFrame complet.
 annee_2021 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup_2021.csv', sep=';')
 annee_2023 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup.csv', sep=';')
-annee_2022 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup_2022.csv', sep=';')
 
 
-# Concatenate annee_2021 and annee_2023 along their columns
-parcoursup_2021_2023 = pd.concat([annee_2021, annee_2023], axis=1)
-parcoursup_2021_2023.to_csv('parcoursup_2021_2023.csv', sep=';', index=False)
+colonnes_communes = annee_2021.columns.intersection(annee_2023.columns)
+
+# Sélectionner les colonnes des deux DataFrames
+annee_2021 = annee_2021[colonnes_communes]
+annee_2023 = annee_2023[colonnes_communes]
+
+# Concaténer en ajoutant les lignes (axis=0)
+concatenated_df = pd.concat([annee_2021, annee_2023], axis=0)
+concatenated_df.to_csv('parcoursup_2021_2023.csv', sep=';', index=False)
