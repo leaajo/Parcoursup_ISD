9791c6218134e31387a85e1b3bb411c115fcf21c
leaajo <lea.joseph@icloud.com>
2024-03-28 14:44:00 +0100

    traitement

--- plot.py
+++ plot.py
@@ -1,21 +1,28 @@
 from math import pi
 
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import plotly.express as px
+
 from bokeh.palettes import Category20c
 from bokeh.plotting import figure, show
 from bokeh.transform import cumsum
 
-lycees_2 = pd.read_csv('./TP_ISD/all_df/lycees_2.csv', sep=';')
+lycees_2 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/lycees_2.csv', sep=',')
 # Compter le nombre d'occurrences de chaque secteur
 secteur_counts = lycees_2['secteur'].value_counts()
 
-# Créer un graphique à secteurs
-plt.figure(figsize=(8, 8))
+# Créer un graphique à secteurs en utilisant pyplot
+plt.figure(figsize=(8, 8))  # Créez une figure en utilisant la fonction figure de pyplot
 plt.pie(secteur_counts, labels=secteur_counts.index, autopct='%1.1f%%', startangle=90, colors=['skyblue', 'salmon'])
 plt.title('Répartition des lycées par secteur')
 plt.axis('equal')  # Assurez-vous que le graphique est un cercle
 plt.show()
+
+fig = px.sunburst(lycees_2, path=['rentree_scolaire', 'secteur'])
+
+# Afficher le graphique interactif
+fig.show()
 
 x = {
     'United States': 157,
