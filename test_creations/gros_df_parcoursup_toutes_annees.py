4b9d0610b236d32f3d9fa745e27f1b465d94e1b4
leaajo <lea.joseph@icloud.com>
2024-03-28 16:39:15 +0100

    .

--- test_creations/gros_df_parcoursup_toutes_annees.py
+++ test_creations/gros_df_parcoursup_toutes_annees.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
-annee_2018 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup-2018.csv', sep=';')
-annee_2019 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup-2019.csv', sep=';')
-annee_2020 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup_2020.csv', sep=';')
+# Avec la réforme du baccalauréat, les données de Parcoursup ont été modifiées depuis 2021. Nous allons donc concaténer les données de 2021,2022 et 2023 pour avoir un DataFrame complet.
 annee_2021 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup_2021.csv', sep=';')
 annee_2023 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup.csv', sep=';')
+annee_2022 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup_2022.csv', sep=';')
 
-parcoursup_total = pd.concat([annee_2018, annee_2019, annee_2020, annee_2021, annee_2023])  # On concatène les données
+annee_2022 = annee_2022.rename(columns=dict(zip(annee_2022.columns, annee_2021.columns.to_list)))
+
+# Concaténer tous les DataFrames en un seul DataFrame
+parcoursup_total = pd.concat([annee_2021, annee_2023], ignore_index=True)
+
 parcoursup_total.to_csv('parcoursup_total.csv', index=False) # Sauvegarde du DataFrame
