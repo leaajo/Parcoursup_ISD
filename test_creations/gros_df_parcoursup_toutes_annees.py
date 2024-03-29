f1906b3e3ed971cf50675ff29e5e2f251f6760d6
leaajo <lea.joseph@icloud.com>
2024-03-29 10:29:40 +0100

    .

--- test_creations/gros_df_parcoursup_toutes_annees.py
+++ test_creations/gros_df_parcoursup_toutes_annees.py
@@ -5,12 +5,6 @@ annee_2021 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/maste
 annee_2023 = pd.read_csv(r'https://raw.githubusercontent.com/leaajo/TP_ISD/master/all_df/fr-esr-parcoursup.csv', sep=';')
 
 
-colonnes_communes = annee_2021.columns.intersection(annee_2023.columns)
 
-# Sélectionner les colonnes des deux DataFrames
-annee_2021 = annee_2021[colonnes_communes]
-annee_2023 = annee_2023[colonnes_communes]
-
-# Concaténer en ajoutant les lignes (axis=0)
-concatenated_df = pd.concat([annee_2021, annee_2023], axis=0)
-concatenated_df.to_csv('parcoursup_2021_2023.csv', sep=';', index=False)
+nouveau_df = pd.concat([annee_2021, annee_2023])
+nouveau_df.to_csv('parcoursup_2021_2023.csv', sep=';', index=False)
